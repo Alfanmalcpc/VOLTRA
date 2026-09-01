841a70c76f4078b14d32bf5f9217b60be2887cde
Alfan <alfanbinkhoirul@gmail.com>
2026-09-01 11:14:56 +0700

    file

--- IKLAN/sw.js
+++ IKLAN/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11696223
+    "zoneId": 11698835
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
